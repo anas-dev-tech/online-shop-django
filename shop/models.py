7eb9247991823c34a3213f47b6170c2aa309d7a3
Anas <tech@tech.tech>
2024-02-25 22:15:08 +0300

    create catalogue template

--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.urls import reverse
+
+
+
+
 class Category(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200,
